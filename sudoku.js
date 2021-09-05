2a801cdbb91db2d1161d2f38e2f1d7bf4f415d7d
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-09-05 18:45:32 -0500

    update sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -1,1 +1,22 @@
+const a = new Array(8);
+const b = new Array(8);
+const c = new Array(8);
+const d = new Array(8);
+const e = new Array(8);
+const f = new Array(8);
+const g = new Array(8);
+const h = new Array(8);
 
+/* set numbers */
+{
+    const tried = new Array(9);
+    while () {
+        var number;
+        while (!tried.contain number) {
+            number = Math.floor(Math.random()*9);
+        }
+    }
+}
+
+/* attempt to unsolve to difficulty */
+//remember variation settings
