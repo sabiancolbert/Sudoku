31c1686db1174b6a2bc968e6a7e6171d6cc0b3f8
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-09-09 22:22:53 -0500

    update sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -1,14 +1,34 @@
-const a = new Array(8);
-const b = new Array(8);
-const c = new Array(8);
-const d = new Array(8);
-const e = new Array(8);
-const f = new Array(8);
-const g = new Array(8);
-const h = new Array(8);
+const X1 = new Array(8);
+const X2 = new Array(8);
+const X3 = new Array(8);
+const X4 = new Array(8);
+const X5 = new Array(8);
+const X6 = new Array(8);
+const X7 = new Array(8);
+const X8 = new Array(8);
 
+const Y1 = new Array(8);
+const Y2 = new Array(8);
+const Y3 = new Array(8);
+const Y4 = new Array(8);
+const Y5 = new Array(8);
+const Y6 = new Array(8);
+const Y7 = new Array(8);
+const Y8 = new Array(8);
 /* set numbers */
 {
+    a1();
+    a2();
+    a3();
+    // use these for midgame checking too
+    
+    
+    
+    
+    
+    
+    
+    
     const tried = new Array();
     var invalid = true;
     while (invalid) {
