4ec500e2920fc3f80ace11906d7fc5b40170d624
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-09-22 18:56:47 -0500

    update sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -488,5 +488,8 @@ var selection = "0";
 //0=erase 1-9=#
 
 function set(cell) {
-    document.getElementById(cell).innerHTML = selection;
+    document.getElementById(cell).innerHTML = "selectionh";
+}
+function onload(){
+    document.getElementById("js").innerHTML = "     1    ";
 }
