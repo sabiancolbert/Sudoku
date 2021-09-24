beff3a9cdb7c2952b17a8b51755191ad27eb3e93
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-09-24 16:59:17 -0500

    update sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -383,107 +383,107 @@ function b1() {
         Y2[0] = number;
     }
 }
+function onload() {
+    //javascript version number for testing
+    document.getElementById("js").innerHTML = "     0    ";
+    /* run cell functions */
+    {
+        a1();
+        a2();
+        a3();
+        a4();
+        a5();
+        a6();
+        a7();
+        a8();
+        a9();
 
-/* run cell functions */
-{
-    a1();
-    a2();
-    a3();
-    a4();
-    a5();
-    a6();
-    a7();
-    a8();
-    a9();
+        b1();
+        b2();
+        b3();
+        b4();
+        b5();
+        b6();
+        b7();
+        b8();
+        b9();
 
-    b1();
-    b2();
-    b3();
-    b4();
-    b5();
-    b6();
-    b7();
-    b8();
-    b9();
+        c1();
+        c2();
+        c3();
+        c4();
+        c5();
+        c6();
+        c7();
+        c8();
+        c9();
 
-    c1();
-    c2();
-    c3();
-    c4();
-    c5();
-    c6();
-    c7();
-    c8();
-    c9();
+        d1();
+        d2();
+        d3();
+        d4();
+        d5();
+        d6();
+        d7();
+        d8();
+        d9();
 
-    d1();
-    d2();
-    d3();
-    d4();
-    d5();
-    d6();
-    d7();
-    d8();
-    d9();
+        e1();
+        e2();
+        e3();
+        e4();
+        e5();
+        e6();
+        e7();
+        e8();
+        e9();
 
-    e1();
-    e2();
-    e3();
-    e4();
-    e5();
-    e6();
-    e7();
-    e8();
-    e9();
+        f1();
+        f2();
+        f3();
+        f4();
+        f5();
+        f6();
+        f7();
+        f8();
+        f9();
 
-    f1();
-    f2();
-    f3();
-    f4();
-    f5();
-    f6();
-    f7();
-    f8();
-    f9();
+        g1();
+        g2();
+        g3();
+        g4();
+        g5();
+        g6();
+        g7();
+        g8();
+        g9();
 
-    g1();
-    g2();
-    g3();
-    g4();
-    g5();
-    g6();
-    g7();
-    g8();
-    g9();
+        h1();
+        h2();
+        h3();
+        h4();
+        h5();
+        h6();
+        h7();
+        h8();
+        h9();
 
-    h1();
-    h2();
-    h3();
-    h4();
-    h5();
-    h6();
-    h7();
-    h8();
-    h9();
-
-    i1();
-    i2();
-    i3();
-    i4();
-    i5();
-    i6();
-    i7();
-    i8();
-    i9();
+        i1();
+        i2();
+        i3();
+        i4();
+        i5();
+        i6();
+        i7();
+        i8();
+        i9();
+    }
+    /* unsolve puzzle */
+    {}
 }
-/* unsolve puzzle */
-{}
 /* -------- */
 /* gameplay */
 /* -------- */
 function set(cell) {
     document.getElementById(cell).innerHTML = document.getElementById("selection").innerHTML;
-}
-function onload() {
-    
 }
