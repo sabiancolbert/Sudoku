f4af3b179dbeec106fff67f226ede14fc1380242
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-09-26 14:06:56 -0500

    update sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -26,7 +26,7 @@ const Y7 = new Array(10);
 const Y8 = new Array(10);
 //0,0 wont be used. starts at 1,1 for simplicity;
 
-const A1tried = new Array(0);
+var A1tried = new Array(0);
 const A2tried = new Array(0);
 const A3tried = new Array(0);
 const A4tried = new Array(0);
