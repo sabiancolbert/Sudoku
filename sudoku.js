a7f4848bfa250c4d8ec6e6719248818ae3b30ee3
sabiancolbert <31480512+sabiancolbert@users.noreply.github.com>
2021-09-28 00:15:23 -0500

    update sudoku.js

--- sudoku.js
+++ sudoku.js
@@ -1,147 +1,67 @@
-/* ------------- */
-/* game creation */
-/* ------------- */
+/* -------------- */
+/* main functions */
+/* -------------- */
 
-/* create variables */
-var number = 0;
-var invalid = true;
+function onload() {
+  //javascript version number for testing
+  document.getElementById("js").innerHTML = version;
+  /* run cell functions */
+  a1();
+  /* unsolve puzzle */
+  {}
+  /* display puzzle */
+  {
+    var counter = 1;
+    function display(row) {
+      for (var i = 1; i < 10; i++) {
+        if (cells[counter] > 0) {
+          document.getElementById(row + i).innerHTML = cells[counter];
+          document.getElementById(row + i).style = "font-weight:bold;";
+          setNumbers.push((row + i));
+        }
+        counter++;
+      }
+    }
+    display("a");
+    display("b");
+    display("c");
+    display("d");
+    display("e");
+    display("f");
+    display("g");
+    display("h");
+    display("i");
+  }
+}
 
-var setNumbers = new Array(0);
-var cells = new Array(82);
+function get(cellX, cellY, x, y) {
+  cellX += x;
+  cellY += y;
+  if (cellX > 0 && cellX < 10 && cellY > 0 && cellY < 10) {
+    if (cellY == 1) {
+      return X1[cellX];
+    } else if (cellY == 2) {
+      return X2[cellX];
+    } else if (cellY == 3) {
+      return X3[cellX];
+    } else if (cellY == 4) {
+      return X4[cellX];
+    } else if (cellY == 5) {
+      return X5[cellX];
+    } else if (cellY == 6) {
+      return X6[cellX];
+    } else if (cellY == 7) {
+      return X7[cellX];
+    } else if (cellY == 8) {
+      return X8[cellX];
+    } else if (cellY == 9) {
+      return X9[cellX];
+    }
+  } else {
+    return 0;
+  }
+}
 
-const X1 = new Array(10);
-const X2 = new Array(10);
-const X3 = new Array(10);
-const X4 = new Array(10);
-const X5 = new Array(10);
-const X6 = new Array(10);
-const X7 = new Array(10);
-const X8 = new Array(10);
-const X9 = new Array(10);
-
-const Y1 = new Array(10);
-const Y2 = new Array(10);
-const Y3 = new Array(10);
-const Y4 = new Array(10);
-const Y5 = new Array(10);
-const Y6 = new Array(10);
-const Y7 = new Array(10);
-const Y8 = new Array(10);
-const Y9 = new Array(10);
-
-//for above, [0,0] wont be used. starts at 1,1 for simplicity;
-
-const A1tried = new Array(0);
-const A2tried = new Array(0);
-const A3tried = new Array(0);
-const A4tried = new Array(0);
-const A5tried = new Array(0);
-const A6tried = new Array(0);
-const A7tried = new Array(0);
-const A8tried = new Array(0);
-const A9tried = new Array(0);
-
-const B1tried = new Array(0);
-const B2tried = new Array(0);
-const B3tried = new Array(0);
-const B4tried = new Array(0);
-const B5tried = new Array(0);
-const B6tried = new Array(0);
-const B7tried = new Array(0);
-const B8tried = new Array(0);
-const B9tried = new Array(0);
-
-const C1tried = new Array(0);
-const C2tried = new Array(0);
-const C3tried = new Array(0);
-const C4tried = new Array(0);
-const C5tried = new Array(0);
-const C6tried = new Array(0);
-const C7tried = new Array(0);
-const C8tried = new Array(0);
-const C9tried = new Array(0);
-
-const D1tried = new Array(0);
-const D2tried = new Array(0);
-const D3tried = new Array(0);
-const D4tried = new Array(0);
-const D5tried = new Array(0);
-const D6tried = new Array(0);
-const D7tried = new Array(0);
-const D8tried = new Array(0);
-const D9tried = new Array(0);
-
-const E1tried = new Array(0);
-const E2tried = new Array(0);
-const E3tried = new Array(0);
-const E4tried = new Array(0);
-const E5tried = new Array(0);
-const E6tried = new Array(0);
-const E7tried = new Array(0);
-const E8tried = new Array(0);
-const E9tried = new Array(0);
-
-const F1tried = new Array(0);
-const F2tried = new Array(0);
-const F3tried = new Array(0);
-const F4tried = new Array(0);
-const F5tried = new Array(0);
-const F6tried = new Array(0);
-const F7tried = new Array(0);
-const F8tried = new Array(0);
-const F9tried = new Array(0);
-
-const G1tried = new Array(0);
-const G2tried = new Array(0);
-const G3tried = new Array(0);
-const G4tried = new Array(0);
-const G5tried = new Array(0);
-const G6tried = new Array(0);
-const G7tried = new Array(0);
-const G8tried = new Array(0);
-const G9tried = new Array(0);
-
-const H1tried = new Array(0);
-const H2tried = new Array(0);
-const H3tried = new Array(0);
-const H4tried = new Array(0);
-const H5tried = new Array(0);
-const H6tried = new Array(0);
-const H7tried = new Array(0);
-const H8tried = new Array(0);
-const H9tried = new Array(0);
-
-const I1tried = new Array(0);
-const I2tried = new Array(0);
-const I3tried = new Array(0);
-const I4tried = new Array(0);
-const I5tried = new Array(0);
-const I6tried = new Array(0);
-const I7tried = new Array(0);
-const I8tried = new Array(0);
-const I9tried = new Array(0);
-
-var co = false;
-var ca = false;
-var no = false;
-
-var sa = false;
-var kr = false;
-var xv = false;
-
-var th = false;
-var pa = false;
-var ar = false;
-
-var di = false;
-var ki = false;
-var kn = false;
-
-
-
-
-
-/* set rules */
 function rules(cell, number) {
   var invalid = false;
   /* consecutive */
@@ -174,8 +94,20 @@ function rules(cell, number) {
 
   return invalid;
 }
+ 
+function set(cell) {
+  /* set cell */
+  if (!setNumbers.includes(cell)) {
+    document.getElementById(cell).innerHTML = document.getElementById("selection").innerHTML;
+  }
+  /* check answer */
+  //HERE
+}
 
-/* create cell functions */
+/* -------------- */
+/* cell functions */
+/* -------------- */
+
 function a1() {
   if (A1tried.length < 9) {
     number = Math.floor(Math.random()*9+1);
@@ -462,75 +394,145 @@ function b2() {
   }
 }
 
-function onload() {
-  //javascript version number for testing
-  document.getElementById("js").innerHTML = version;
-  /* run cell functions */
-  a1();
-  /* unsolve puzzle */
-  {}
-  /* display puzzle */
-  {
-    var counter = 1;
-    function display(row) {
-      for (var i = 1; i < 10; i++) {
-        if (cells[counter] > 0) {
-          document.getElementById(row + i).innerHTML = cells[counter];
-          document.getElementById(row + i).style = "font-weight:bold;";
-          setNumbers.push((row + i));
-        }
-        counter++;
-      }
-    }
-    display("a");
-    display("b");
-    display("c");
-    display("d");
-    display("e");
-    display("f");
-    display("g");
-    display("h");
-    display("i");
-  }
-}
+/* --------- */
+/* variables */
+/* --------- */
 
-/* -------- */
-/* gameplay */
-/* -------- */
-function set(cell) {
-  /* set cell */
-  if (!setNumbers.includes(cell)) {
-    document.getElementById(cell).innerHTML = document.getElementById("selection").innerHTML;
-  }
-  /* check answer */
-  //HERE
-}
+/* misc */
+var number = 0;
+var invalid = true;
+var setNumbers = new Array(0);
 
-function get(cellX, cellY, x, y) {
-  cellX += x;
-  cellY += y;
-  if (cellX > 0 && cellX < 10 && cellY > 0 && cellY < 10) {
-    if (cellY == 1) {
-      return X1[cellX];
-    } else if (cellY == 2) {
-      return X2[cellX];
-    } else if (cellY == 3) {
-      return X3[cellX];
-    } else if (cellY == 4) {
-      return X4[cellX];
-    } else if (cellY == 5) {
-      return X5[cellX];
-    } else if (cellY == 6) {
-      return X6[cellX];
-    } else if (cellY == 7) {
-      return X7[cellX];
-    } else if (cellY == 8) {
-      return X8[cellX];
-    } else if (cellY == 9) {
-      return X9[cellX];
-    }
-  } else {
-    return 0;
-  }
-}
-var version = 99;
+/* rules */
+var co = false;
+var ca = false;
+var no = false;
+
+var sa = false;
+var kr = false;
+var xv = false;
+
+var th = false;
+var pa = false;
+var ar = false;
+
+var di = false;
+var ki = false;
+var kn = false;
+
+/* board */
+
+var cells = new Array(82);
+
+//0,0 wont be used (for simplicity)
+var X1 = new Array(10);
+var X2 = new Array(10);
+var X3 = new Array(10);
+var X4 = new Array(10);
+var X5 = new Array(10);
+var X6 = new Array(10);
+var X7 = new Array(10);
+var X8 = new Array(10);
+var X9 = new Array(10);
+
+var Y1 = new Array(10);
+var Y2 = new Array(10);
+var Y3 = new Array(10);
+var Y4 = new Array(10);
+var Y5 = new Array(10);
+var Y6 = new Array(10);
+var Y7 = new Array(10);
+var Y8 = new Array(10);
+var Y9 = new Array(10);
+
+/* construction */
+
+var A1tried = new Array(0);
+var A2tried = new Array(0);
+var A3tried = new Array(0);
+var A4tried = new Array(0);
+var A5tried = new Array(0);
+var A6tried = new Array(0);
+var A7tried = new Array(0);
+var A8tried = new Array(0);
+var A9tried = new Array(0);
+
+var B1tried = new Array(0);
+var B2tried = new Array(0);
+var B3tried = new Array(0);
+var B4tried = new Array(0);
+var B5tried = new Array(0);
+var B6tried = new Array(0);
+var B7tried = new Array(0);
+var B8tried = new Array(0);
+var B9tried = new Array(0);
+
+var C1tried = new Array(0);
+var C2tried = new Array(0);
+var C3tried = new Array(0);
+var C4tried = new Array(0);
+var C5tried = new Array(0);
+var C6tried = new Array(0);
+var C7tried = new Array(0);
+var C8tried = new Array(0);
+var C9tried = new Array(0);
+
+var D1tried = new Array(0);
+var D2tried = new Array(0);
+var D3tried = new Array(0);
+var D4tried = new Array(0);
+var D5tried = new Array(0);
+var D6tried = new Array(0);
+var D7tried = new Array(0);
+var D8tried = new Array(0);
+var D9tried = new Array(0);
+
+var E1tried = new Array(0);
+var E2tried = new Array(0);
+var E3tried = new Array(0);
+var E4tried = new Array(0);
+var E5tried = new Array(0);
+var E6tried = new Array(0);
+var E7tried = new Array(0);
+var E8tried = new Array(0);
+var E9tried = new Array(0);
+
+var F1tried = new Array(0);
+var F2tried = new Array(0);
+var F3tried = new Array(0);
+var F4tried = new Array(0);
+var F5tried = new Array(0);
+var F6tried = new Array(0);
+var F7tried = new Array(0);
+var F8tried = new Array(0);
+var F9tried = new Array(0);
+
+var G1tried = new Array(0);
+var G2tried = new Array(0);
+var G3tried = new Array(0);
+var G4tried = new Array(0);
+var G5tried = new Array(0);
+var G6tried = new Array(0);
+var G7tried = new Array(0);
+var G8tried = new Array(0);
+var G9tried = new Array(0);
+
+var H1tried = new Array(0);
+var H2tried = new Array(0);
+var H3tried = new Array(0);
+var H4tried = new Array(0);
+var H5tried = new Array(0);
+var H6tried = new Array(0);
+var H7tried = new Array(0);
+var H8tried = new Array(0);
+var H9tried = new Array(0);
+
+var I1tried = new Array(0);
+var I2tried = new Array(0);
+var I3tried = new Array(0);
+var I4tried = new Array(0);
+var I5tried = new Array(0);
+var I6tried = new Array(0);
+var I7tried = new Array(0);
+var I8tried = new Array(0);
+var I9tried = new Array(0);
